da2cf3b9f1eaf615a0e9e91dada576db86982eda
Heranca <heranca.kamalo@gmail.com>
2021-05-02 00:05:30 +0200

    create react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { 
+
   return (
     <div className="App">
+
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
