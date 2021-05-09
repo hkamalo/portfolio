47f948619beb2b89973dd9de3ff1555f6ad9cdec
Heranca <heranca.kamalo@gmail.com>
2021-05-09 22:22:32 +0200

    first change on background section

--- src/components/ForHomePage/Background.js
+++ src/components/ForHomePage/Background.js
@@ -71,9 +71,40 @@ export default function Background() {
   return (
     <>
       <div className={classes.skillsElements}>
-        <p>Diplome</p>
-        <p>Profesionnel</p>
-        <p>Skills</p>
+        
+        <div>
+          <h4>Diplome</h4>
+          <div>
+            <p>Master Management & Stratégie d’Entreprise</p>
+          </div>
+        </div>
+        <div>
+          <h4>Professional</h4>
+          <div>
+            <p><strong>Business Developer</strong> - 2 ans</p>
+            <p><strong>Commercial Itinérant</strong> - 1 an</p>
+          </div>
+        </div>
+        <div>
+          <h4>Acquis</h4>
+          <div className={classes.hardSKills}>
+            <h5>Savoir faire</h5>
+            <ul>
+              <li>Pilotage d’un projet</li>
+              <li>Relationnelle client</li>
+              <li>Travail en équipe</li>
+              <li>Maîtrise de l’anglais</li>
+            </ul>
+          </div>
+          <div className={classes.softSkills}>
+            <h5>Savoir être</h5>
+            <ul>
+              <li>Entrepreneur</li>
+              <li>Rigoureux</li>
+              <li>Sociable</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
