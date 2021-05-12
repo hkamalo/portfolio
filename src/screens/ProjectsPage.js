2a619a3a91c9711f8ecbe0c54b5d93541b9ef910
Heranca <heranca.kamalo@gmail.com>
2021-05-12 17:25:58 +0200

    add change on footer and project

--- src/screens/ProjectsPage.js
+++ src/screens/ProjectsPage.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles({
     margin: 20,
     paddingTop: 50,
     paddingBottom: 50,
-    borderRadius: 10
+    borderRadius: 10,
   },
   projectsDisplay: {
     display: 'flex',
